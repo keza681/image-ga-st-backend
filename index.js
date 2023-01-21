3b9e0cb6aa87a9743754a169db4db983f6b288cb
keza681 <lindakeza19@gmail.com>
2023-01-21 18:54:02 +0200

    AImplement the middleware

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,10 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 8000;
